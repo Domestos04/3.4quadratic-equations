1e956d4af9ff8dd82295ca4129b549174a388661
Domestos04 <klimenkoantonkhua@gmail.com>
2022-09-16 15:22:57 +0300

    3.4quadratic equations

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// a*x2 + b*x +c = 0
+$x = rand(-10, 10);
+
+$a = rand(-10, 10);
+$b = rand(-10, 10);
+$c = rand(-10, 10);
+
+if ($x != 0){
+    $x = $a - $b - $c;
+    echo "<span style = \" color: BlueViolet;\">Квадратне рівняня:</span>". $x. " ";  
+}else {
+    echo "Помилка";
+ }
+
+?>
