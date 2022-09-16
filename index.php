7947b40da9bd67edc81a8dca4020851844a6e86b
Domestos04 <klimenkoantonkhua@gmail.com>
2022-09-16 22:37:54 +0300

    .

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
 // a*x2 + b*x +c = 0
 $x = rand(-10, 10);
-
 $a = rand(-10, 10);
 $b = rand(-10, 10);
 $c = rand(-10, 10);
 
 if ($x != 0){
-    $x = $a - $b - $c;
+    // if($x != 0){
+    $x = $b * $b - 4 * $a * $c;
     echo "<span style = \" color: BlueViolet;\">Квадратне рівняня:</span>". $x. " ";  
+// }
 }else {
     echo "Помилка";
  }
